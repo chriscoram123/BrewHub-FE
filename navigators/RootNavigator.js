da4e9e6947047c60ee3d030b1c86b415b24f0bd3
Christopher Coram <ultraartist15@gmail.com>
2023-11-07 11:11:45 -0500

    Root Navigation installation complete

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,2 +1,33 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// import App from "../App"
+
+// Light Mode Screens
+import LiquoresPage from "../screens/light_mode/liquores_page"
+import BaseLiquoresPage from "../screens/light_mode/base_liquores_page"
+import MartinsPage from "../screens/light_mode/martins_page"
+import LightModeMenu from "../screens/light_mode/light_mode_menu"
+
+// Dark Mode Screens
+// import LiquoresPage from "../screens/dark_mode/liquores_page"
+// import BaseLiquoresPage from "../screens/dark_mode/base_liquores_page"
+// import MartinsPage from "../screens/dark_mode/martins_page"
+// import DarkModeMenu from "../screens/dark_mode/light_mode_menu"
+
+
+const Stack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+    return (
+        <Stack.Navigator initialRouteName="Liquores">
+            {/* <Stack.Screen name="App" component={App} /> */}
+            <Stack.Screen name="Liquores" component={LiquoresPage} />
+            <Stack.Screen name="BaseLiquores" component={BaseLiquoresPage} />
+            <Stack.Screen name="Martins" component={MartinsPage} />
+            <Stack.Screen name="LightModeSwitch" component={LightModeMenu} />
+        </Stack.Navigator>
+    );
+}
+
+export default RootNavigator;

3fff1173e10f7aea6f06f132e03d0724b65216a9
Christopher Coram <ultraartist15@gmail.com>
2023-11-13 07:07:02 -0500

    Edits made to header and scrollview

--- screens/light_mode/liquores_page.js
+++ screens/light_mode/liquores_page.js
@@ -5,7 +5,7 @@ import Hamburger from 'hamburger-react'
 export default function LiquoresPage({ navigation: { navigate } }) {
     
     // UseState for hamburger menu
-    // const [isOpen, setOpen] = useState(false)
+    const [isOpen, setOpen] = useState(false)
 
     return (
         <View>
@@ -14,25 +14,26 @@ export default function LiquoresPage({ navigation: { navigate } }) {
                 {/* logo header container */}
                 <View style={styles.logoImgCon}> 
                     <Image 
-                        // source={require('../../assets/icon.png')}
-                        // style={styles.logoImage}
-                        // resizeMode='center'
+                        source={require('../../assets/icon.png')}
+                        style={styles.logoImage}
+                        resizeMode='center'
                     />
                 </View>
-
-                {/* Header Text */}
-                <Text> BrewHub </Text>
-
-                {/* Hamburger Menu Icon
-                <View style={styles.menuCon}>
-                    <Hamburger toggled={isOpen} toggle={setOpen} />
-                </View> */}
+                
+                {/* Hamburger Menu Icon */}
+                <View style={styles.menuImgCon}> 
+                    <Image 
+                        source={require('../../assets/icon.png')}
+                        style={styles.menuImage}
+                        resizeMode='center'
+                    />
+                </View>
             </View>
 
 
 
             <View style={styles.scrollViewCon}>
-                <ScrollView horizontal={true} style={{ flex: 1, paddingVertical: 100,}}>
+                <ScrollView horizontal={true} style={{ flex: 1, paddingVertical: 30,}}>
                     
                     <View style={styles.iconCon}>
 
@@ -93,10 +94,11 @@ export default function LiquoresPage({ navigation: { navigate } }) {
 const styles = StyleSheet.create({
     headerCon: {
         flexDirection: "row",
-        justifyContent: "center",
+        justifyContent: "space-between",
         backgroundColor: "blue",
         marginVertical: 10,
         paddingVertical: 20,
+        paddingHorizontal: 30,
     },
     scrollViewCon: {
         backgroundColor: "purple",
@@ -105,11 +107,15 @@ const styles = StyleSheet.create({
     articleViewCon: {
         marginVertical: 20,
     },
+    iconCon: {
+        // paddingVertical:20,
+        backgroundColor: "green",
+    },
     cardConOne: {
-        // maxHeight: 100,
+        maxHeight: 100,
         // maxWidth: 50,
-        // paddingHorizontal: 100,
-        justifyContent: "center",
+        paddingHorizontal: 40,
+        // justifyContent: "center",
         // flex: 1,
     },
     menuCon: {
@@ -118,10 +124,23 @@ const styles = StyleSheet.create({
     iconImage: {
         maxWidth: 100,
     },
+    logoImage: {
+        maxHeight: 30,
+        maxWidth: 90,
+    },
     logoImgCon: {
         // flexDirection: "row",
         // flex:1,
-        // maxHeight: 100,
+        maxHeight: 20,
+        marginBottom: 10,
+    },
+    menuImage: {
+        maxHeight: 30,
+        maxWidth: 50,
+    },
+    menuImgCon: {
+        maxHeight: 20,
+        marginBottom: 10,
     },
 
     imageBackground: {
